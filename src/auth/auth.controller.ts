910c047d2656ea893fe9b0c79749e5e3875de021
sollertia <sollertia331@gmail.com>
2023-03-17 12:28:14 +0900

    커스텀 데코레이터를 사용하여 User 정보만 뽑아오기

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -10,6 +10,8 @@ import {
 import { AuthService } from './auth.service';
 import { AuthCredentialDto } from './dto/auth-credential.dto';
 import { AuthGuard } from '@nestjs/passport';
+import { GetUser } from './get-user.decorator';
+import { User } from './user.entity';
 
 @Controller('auth')
 export class AuthController {
@@ -30,8 +32,10 @@ export class AuthController {
   }
 
   @Get('/auth_test')
-  @UseGuards(AuthGuard()) // user
-  authTest(@Req() req) {
-    console.log('req', req);
+  @UseGuards(AuthGuard())
+  authTest(@GetUser() user: User) {
+    // 커스텀 데코레이터 사용
+    console.log('user: ', user);
   }
+  기;
 }
