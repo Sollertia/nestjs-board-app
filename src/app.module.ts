e7081824995013430d1335333a80fe9354eea3ad
sollertia <sollertia331@gmail.com>
2023-03-17 10:49:40 +0900

    Auth 모듈 추가 & 회원가입 api 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { BoardModule } from './board/boardModule';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeOrmConfig } from './config/typeorm.comfig';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(typeOrmConfig), BoardModule],
+  imports: [TypeOrmModule.forRoot(typeOrmConfig), BoardModule, AuthModule],
 })
 export class AppModule {}
