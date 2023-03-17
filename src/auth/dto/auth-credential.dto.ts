10b50ce846cb59592203aaa35679e7862e9bb879
sollertia <sollertia331@gmail.com>
2023-03-17 10:53:32 +0900

    User Req Data 유효성 추가

--- src/auth/dto/auth-credential.dto.ts
+++ src/auth/dto/auth-credential.dto.ts
@@ -1,4 +1,14 @@
+import { IsString, Matches, MaxLength, MinLength } from 'class-validator';
+
 export class AuthCredentialDto {
+  @IsString()
+  @MinLength(4)
+  @MaxLength(14)
   username: string;
+
+  @IsString()
+  @MinLength(4)
+  @MaxLength(14)
+  @Matches(/^[a-zA-Z0-9]*$/) // 영문, 숫자 가능
   password: string;
 }
