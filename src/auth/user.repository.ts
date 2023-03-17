529cf13829d6a3804fcd77c941feb37ccf691324
sollertia <sollertia331@gmail.com>
2023-03-17 11:06:58 +0900

    @Unique 사용하여 username 고유값 부여! 및 예외처리

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -2,12 +2,28 @@ import { CustomRepository } from '../orm/typeorm-ex.decorator';
 import { Repository } from 'typeorm';
 import { User } from './user.entity';
 import { AuthCredentialDto } from './dto/auth-credential.dto';
+import {
+  ConflictException,
+  InternalServerErrorException,
+} from '@nestjs/common';
 
 @CustomRepository(User)
 export class UserRepository extends Repository<User> {
   async createUser(authCredentialDto: AuthCredentialDto): Promise<void> {
     const { username, password } = authCredentialDto;
     const user = this.create({ username, password });
-    await this.save(user);
+
+    try {
+      await this.save(user);
+    } catch (error) {
+      if (error.code === '23505') {
+        // code: '23505'
+        throw new ConflictException(
+          `${username} 는(은) 존재합니다. 다른 username을 입력하세요!`,
+        );
+      } else {
+        throw new InternalServerErrorException();
+      }
+    }
   }
 }
